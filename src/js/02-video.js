37e046ec79d97fa547dadc0165462e41ccd1e0f8
RomanPiat <108830304+RomanPiat@users.noreply.github.com>
2022-12-08 22:28:08 +0200

    home_work_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeItem = JSON.parse(localStorage.getItem(TIME_KEY));
+
+function playerVimeo() {
+   if (timeItem !== null) {
+  player.setCurrentTime(timeItem);
+  };
+  player.on('timeupdate', throttle((timeData) => 
+    localStorage.setItem(TIME_KEY, JSON.stringify(timeData.seconds)), 1000
+  ));
+};
+
+playerVimeo();
